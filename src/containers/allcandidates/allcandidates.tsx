b40eae441a286393ddddad13eeff1056deaf4e16
shudhanshu0011 <shudhanshu0011@gmail.com>
2023-08-23 09:50:28 +0530

    Form for Post New Job Done

--- src/containers/allcandidates/allcandidates.tsx
+++ src/containers/allcandidates/allcandidates.tsx
@@ -1,5 +1,5 @@
 
-import { Container, Row, Col } from "react-bootstrap";
+import { Row, Col } from "react-bootstrap";
 import { SelectDropdown } from "@common/select";
 import { Paper } from "@common/Paper";
 import { AppPagination } from "@common/app-pagination";
@@ -9,13 +9,14 @@ import "@styles/common/_pages.scss";
 import { useGetCandidates } from "@hooks/useGetCandidates";
 import "./allcandidates.scss";
 import { Sidebar } from "../../components/sidebar/sidebar";
-
+import { useState } from "react";
+import { CandidateDetails } from "@components/candidate-detail/candidate-detail";
+import { Btn } from "@src/common/button";
+import "@styles/common/_pages.scss";
 
 export const AllCandidates = (): JSX.Element => {
-  const {data: candidateData} = useGetCandidates();
-
-  
-
+  const { data: candidateData } = useGetCandidates();
+  const [showDetail, setShowDetail] = useState(false);
 
   const options = [
     { value: ".NET", label: ".NET" },
@@ -36,62 +37,76 @@ export const AllCandidates = (): JSX.Element => {
     ];
     return <SelectDropdown options={options} size="sm" />;
   };
+
+  const handleShowDetails = (isVisible: boolean) => {
+    setShowDetail(isVisible);
+  };
   return (
     <PageWrapper>
-      <div className="body-items">
-        <Sidebar activeLink="/allcandidates"/>
-        <div className="box-content pt-40 pl-30">
-              <h3 className="mb-35">Candidates</h3>
-              <Paper title="Advance Filter" titleRight="Search Result : 5">
-                <div className="filter-dropdown-container">
-                  <Row>
-                    <Col xs={3}>
-                      <SelectDropdown
-                        options={options}
-                        placeholder="Select Technology"
-                        isClearable
-                        size="lg"
-                      />
-                    </Col>
-                    <Col xs={3}>
-                      <SelectDropdown
-                        options={options}
-                        placeholder="Select Location"
-                        size="lg"
-                        isClearable
-                      />
-                    </Col>
-                  </Row>
-                </div>
-              </Paper>
-              <Paper
-                title={candidatePagination()}
-                titleRight={pageViewDropdown()}
-              >
-                <div className="flex-box candidate-card-wrapper">
-                  <div className="candidate-card-item">
-                    <CandidateCard />
+        <div className="body-items">
+          <Sidebar activeLink="/allcandidates"/>
+          {showDetail ? (
+            <CandidateDetails handleShowDetails={handleShowDetails} />
+          ) : (
+            <Col xs={12} md={10}>
+              <div className="box-content pt-40 pl-30">
+                <h3 className="mb-35">Candidates</h3>
+                <Paper title="Advance Filter" titleRight="Search Result : 5">
+                  <div className="filter-dropdown-container">
+                    <Row>
+                      <Col xs={3}>
+                        <SelectDropdown
+                          options={options}
+                          placeholder="Select Technology"
+                          isClearable
+                          size="lg"
+                        />
+                      </Col>
+                      <Col xs={3}>
+                        <SelectDropdown
+                          options={options}
+                          placeholder="Select Location"
+                          size="lg"
+                          isClearable
+                        />
+                      </Col>
+                    </Row>
                   </div>
+                </Paper>
+                <Paper
+                  title={candidatePagination()}
+                  titleRight={pageViewDropdown()}
+                >
+                  <div className="flex-box candidate-card-wrapper">
+                    <div className="candidate-card-item">
+                      <CandidateCard handleShowDetails={handleShowDetails} />
+                    </div>
 
-                  <div className="candidate-card-item">
-                    <CandidateCard />
+                    <div className="candidate-card-item">
+                      <CandidateCard handleShowDetails={handleShowDetails} />
+                    </div>
+                    <div className="candidate-card-item">
+                      <CandidateCard handleShowDetails={handleShowDetails} />
+                    </div>
+                    <div className="candidate-card-item">
+                      <CandidateCard handleShowDetails={handleShowDetails} />
+                    </div>
+                    <div className="candidate-card-item">
+                      <CandidateCard handleShowDetails={handleShowDetails} />
+                    </div>
+                    <div className="candidate-card-item">
+                      <CandidateCard handleShowDetails={handleShowDetails} />
+                    </div>
                   </div>
-                  <div className="candidate-card-item">
-                    <CandidateCard />
-                  </div>
-                  <div className="candidate-card-item">
-                    <CandidateCard />
-                  </div>
-                  <div className="candidate-card-item">
-                    <CandidateCard />
-                  </div>
-                  <div className="candidate-card-item">
-                    <CandidateCard />
-                  </div>
-                </div>
-              </Paper>
-            </div>
-      </div>
+                  <Btn
+                    title="Login to see more"
+                    className="btn btn-default btn-apply font-sm"
+                  />
+                </Paper>
+              </div>
+            </Col>
+          )}
+        </div>
     </PageWrapper>
   );
 };
--- src/containers/postjob/postjob.tsx
+++ src/containers/postjob/postjob.tsx
@@ -1,19 +1,234 @@
-
+import React, { useEffect, useState } from "react";
 import { PageWrapper } from "@components/page-wrapper/page-wrapper";
+import { Sidebar } from "../../components/sidebar/sidebar";
+import { Row, Col } from "react-bootstrap";
 import "@styles/common/_pages.scss";
 import "./postjob.scss";
-import { Sidebar } from "../../components/sidebar/sidebar";
+import axios from "axios";
 
+const defaultFormData = {
+    duration: 0,
+    skill: "",
+    JobHeading: "",
+    location: "",
+    openpositions: 0,
+    experienceLevel: 0,
+    startDate: "",
+    hourlyPrice: 0,
+    description: "",
+    jobStatus: "",
+};
 
 export const PostJob = (): JSX.Element => {
+  // const [locationList, setLocationList] = useState([{'cityName':'', 'cityId': ''}])
+
+  // useEffect(() => {
+  //   const fetchData =async () => {
+  //     const response = await fetch(`http://localhost:3001/v1/joblocation`);
+  //     const newData = await response.json();
+  //     setLocationList(newData);
+  //     console.log(newData);
+  //   };
+  //   fetchData();
+  // }, [])
+
+  const locationList = [
+    {
+      cityName: "Delhi",
+      cityId: 1,
+    },
+    {
+      cityName: "Guragon",
+      cityId: 2,
+    },
+    {
+      cityName: "Hyderabad",
+      cityId: 3,
+    },
+    {
+      cityName: "Bangalore",
+      cityId: 4,
+    }
+  ];
+
+  const skillList = [
+    {
+      skillName: "React",
+      skillId: 1,
+    },
+    {
+      skillName: "Python",
+      skillId: 2
+    },
+    {
+      skillName: "Django",
+      skillId: 3
+    },
+    {
+      skillName: "Node Js",
+      skillId: 4,
+    },
+  ];
+
+  const statusList = [
+    {
+      statusName: "New",
+      statusId: 1,
+    },
+    {
+      statusName: "Assigned",
+      statusId: 2,
+    },
+    {
+      statusName: "Profile Shared",
+      statusId: 3,
+    },
+    {
+      statusName: "Interviews",
+      statusId: 4,
+    },
+    {
+      statusName: "Selected",
+      statusId: 5,
+    },
+    {
+      statusName: "Won",
+      statusId: 6,
+    },
+    {
+      statusName: "Lost",
+      statusId: 7,
+    },
+    {
+      statusName: "Hold",
+      statusId: 8,
+    },
+  ];
+
+  const [formData, setFormData] = useState(defaultFormData);
+  const {duration,
+         skill,
+         JobHeading,
+         location,
+         openpositions,
+         experienceLevel,
+         startDate,
+         hourlyPrice,
+         description,
+         jobStatus } = formData;
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  };
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(formData);
+    setFormData(defaultFormData);
+    // fetch("http://localhost:3001/v1/job", {
+    //   method: 'POST',
+    //   body: JSON.stringify({
+    //     formData
+    //   })
+    // }).then((response) => response.json()).then((data) => {
+    //   console.log(data)
+    // })
+  };  
+
   return (
     <PageWrapper>
       <div className="body-items">
         <Sidebar activeLink="/newjob"/>
-        <div className="box-content pt-40 pl-30">
-          Post Job
-        </div>
-      </div>
+        <Col xs={12} md={10}>
+              <div className="box-content pt-40 pl-30">
+                <h3 className="mb-35">Post a New Job</h3>
+                <div className="form-card">
+                  <form onSubmit={onSubmit}>
+                    <div className="form">
+                      <div className="coll-1">
+                          <Row>
+                            <label>Job Title*</label>
+                            <input type="text" id="JobHeading" value={JobHeading} placeholder="Enter Title" onChange={onChange} />
+                          </Row>
+                          <Row>
+                            <label>Locations</label>
+                            <select className="basic-single" type="text" id="location" value={location} placeholder="Location" onChange={onChange}>
+                              <option selected disabled hidden>Select an Option</option>
+                              {
+                                locationList.map(locations => (
+                                  <option value={locations.cityId}>{locations.cityName}</option>
+                                ))
+                              }
+                            </select>
+                          </Row>
+                          <Row>
+                            <label>Skills</label>
+                            <select type="text" id="skill" value={skill} placeholder="Skill" onChange={onChange}>
+                            <option selected disabled hidden>Select an Option</option>
+                              {
+                                skillList.map(skills => (
+                                  <option value={skills.skillId}>{skills.skillName}</option>
+                                ))
+                              }
+                            </select>
+                          </Row>
+                          <Row>
+                            <label>Summary</label>
+                            <textarea rows={6} id="description" value={description} placeholder="Description" onChange={onChange}></textarea>
+                          </Row>
+                      </div>
+                      <div className="coll-2">
+                        <div className="coll-2-card">
+                          <div className="coll-2-1">
+                                <Row>
+                                  <label>Monthly Budget (INR)*</label>
+                                  <input type="number" id="hourlyPrice" value={hourlyPrice} placeholder="Enter Monthly Budget" onChange={onChange} />
+                                </Row>
+                                <Row>
+                                  <label>Project Start Date*</label>
+                                  <input type="date" id="startDate" value={startDate} placeholder="Select a date" onChange={onChange} />
+                                </Row>
+                                <Row>
+                                  <label>Status</label>
+                                  <select type="text" id="status" value={jobStatus} placeholder="Select Status" onChange={onChange}>
+                                  <option selected disabled hidden>Select an Option</option>
+                                    {
+                                      statusList.map(statuss => (
+                                        <option value={statuss.statusId}>{statuss.statusName}</option>
+                                      ))
+                                    }
+                                  </select>
+                                </Row>
+                          </div>
+                          <div className="coll-2-2">
+                                <Row>
+                                  <label>Minimum Experience*</label>
+                                  <input type="number" id="experienceLevel" value={experienceLevel} placeholder="Minimum Experience (in years)" onChange={onChange} />
+                                </Row>
+                                <Row>
+                                  <label>Duration in Months* </label>
+                                  <input type="number" id="duration" value={duration} placeholder="Project Duration in Months " onChange={onChange} />
+                                </Row>
+                                <Row>
+                                  <label>Open positions*</label>
+                                  <input type="number" id="openpositions" value={openpositions} placeholder="Enter No. Position" onChange={onChange} />
+                                </Row>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="submitButton">
+                      <button type="submit" className="submit-btn"><span>Submit</span></button>
+                    </div>
+                  </form>
+                  
+                </div>
+                </div>
+            </Col>
+      </div>  
     </PageWrapper>
   );
 };
