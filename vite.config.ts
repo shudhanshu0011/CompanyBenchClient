4939db17721942c3ace9fda39cdffc2ed20fde03
Abhishek Bhardwaj <abhishek.hcst14@gmail.com>
2023-08-30 01:49:29 +0530

    vite config update

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,6 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, 'index.html'),
-        nested: path.resolve(__dirname, 'nested/index.html'),
-      },
-    },
-  },
   plugins: [react(), svgr()],
   resolve: {
     alias: {
