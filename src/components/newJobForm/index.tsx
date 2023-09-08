84382c7f343613212cfd5c134bc93da77b65e07f
shudhanshu0011 <shudhanshu0011@gmail.com>
2023-09-09 00:43:27 +0530

    Fixed

--- src/components/newJobForm/index.tsx
+++
@@ -1,1 +0,0 @@
-export * from './new-job-form'
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,14 +1,6 @@
 import { RouteObject } from "react-router-dom";
 import { Home } from "./containers/home";
 import { Candidates } from "./containers/candidates";
-import { AllCandidates } from "./containers/allcandidates/allcandidates";
-import { Dashboard } from "./containers/dashboard/dashboard";
-import { AppliedCandidates } from "./containers/appliedcandidates/appliedcandidates";
-import { InterviewList } from "./containers/interview/interview";
-import { MatchingCandidates } from "./containers/matchingcandidates/matchingcandidates";
-import { MyJobs } from "./containers/myjobs/myjobs";
-import { PostJob } from "./containers/postjob/postjob";
-import { Shortlisted } from "./containers/shortlisted/shortlisted";
 import { Sidebar } from "./components/sidebar/sidebar";
 import { Jobs } from "./containers/navigation-pages/jobs";
 import { ApplyForJob } from "./containers/navigation-pages/apply-for-job";
@@ -25,38 +17,6 @@ const routes: RouteObject[] = [
   {
     path: "/c/candidates",
     element: <Candidates />,
-  },
-  {
-    path: "/allcandidates",
-    element: <AllCandidates />,
-  },
-  {
-    path: "/c/dashboard",
-    element: <Dashboard />,
-  },
-  {
-    path: "/interestedcandidate",
-    element: <AppliedCandidates />,
-  },
-  {
-    path: "/jobinterviewlist",
-    element: <InterviewList />,
-  },
-  {
-    path: "/cmatchingcandidate",
-    element: <MatchingCandidates />,
-  },
-  {
-    path: "/cmyjobs",
-    element: <MyJobs />,
-  },
-  {
-    path: "/newjob",
-    element: <PostJob />,
-  },
-  {
-    path: "/shortlistedcandidates",
-    element: <Shortlisted />,
   },
   {
     path: "/",
